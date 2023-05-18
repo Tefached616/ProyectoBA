dc380d21dafca53e568cdfb6037b9f9da7173976
Tefached616 <afx3v6gmail.com>
2023-05-18 17:20:00 -0500

    terminado el modulo produccion por materia prima

--- src/Modelos/ProductoxMateriaprimaModelo.js
+++ src/Modelos/ProductoxMateriaprimaModelo.js
@@ -1,93 +1,70 @@
 //#region METODO LISTAR
-const connection = require('../conexion');
+const { response } = require("express");
+const { connection, makeQuery } = require("../conexion/index.js");
 
 var ProductoxMateriaprimaModelo = {};
 
-ProductoxMateriaprimaModelo.getProductoxMateriaprimas = function(callback)
-{
-    if(connection)
-    {
-    var sql = "SELECT F. `ID_PROD_X_MAT`,    MP. `NOM_MATERIA_PRIMA`,    P. `NOM_PRODUCTO`    FROM `productoxmateriaprima` AS F   	INNER JOIN `materia_prima` AS MP ON F. `ID_MATERIA_PRIMA` = MP. `ID_MATERIA_PRIMA`"+
+const getProductoxMateriaprimas = (req, res = response) => {
+  var sql =
+    "SELECT F. `ID_PROD_X_MAT`, MP. `NOM_MATERIA_PRIMA`, P. `NOM_PRODUCTO` FROM `productoxmateriaprima` AS F INNER JOIN `materia_prima` AS MP ON F. `ID_MATERIA_PRIMA` = MP. `ID_MATERIA_PRIMA`" +
     " INNER JOIN `producto` AS P ON F. `ID_PRODUCTO` = P. `ID_PRODUCTO` ORDER BY `ID_PROD_X_MAT`";
 
-    connection.query(sql, function(error, rows)
-    {
-        if(error)
-        {
-            throw error;
-        }
-            else
-        {
-            callback(null, rows);
-        }
-    });
-}
-}
-
-module.exports =ProductoxMateriaprimaModelo;
-//#endregion
-//#region METODO INSERTAR
-ProductoxMateriaprimaModelo.insertProductoxMateriaprima = function (ProdXMPData, callback) {
-    if (connection) {
-        var sql = " INSERT INTO productoxmateriaprima SET ?";
-
-        connection.query(sql, ProdXMPData, function (error, result) {
-            console.log(" 44 productoxmateriaprima "+ProdXMPData.ID_MATERIA_PRIMA+" ini "
-            +ProdXMPData.ID_PRODUCTO
-            );
-            if (error) {
-                throw error;
-
-            } else {
-                callback(null, { "msg": "Registro insertado" });
-            }
-        });
-    }
+    makeQuery(sql).then((result) => res.json(result)).catch((err) => res.status(500).json(err));
 };
-//#endregion
-//#region METODO MODIFICAR
-ProductoxMateriaprimaModelo.updateProductoxMateriaprima = function(ProdXMPData, callback){
-    if(connection){
-        var sql = "UPDATE productoxmateriaprima SET ID_MATERIA_PRIMA = "+
-        connection.escape(ProdXMPData.ID_MATERIA_PRIMA)
-        +", ID_PRODUCTO = "+
-        connection.escape(ProdXMPData.ID_PRODUCTO)
-        +" WHERE ID_PROD_X_MAT = "+
-        connection.escape(ProdXMPData.ID_PROD_X_MAT)+";";
-        
-        connection.query(sql, function(error, result){
-            if(error){
-                throw error;
-            }else{
-                callback(null, {"msg:": "Registro Actualizado"});
-            }
-        });
-    }
-}
+
+const insertProductoxMateriaprima = (req, res = response) => {
+
+    var ProdXMPData = {
+        ID_MATERIA_PRIMA: req.body.ID_MATERIA_PRIMA,
+        ID_PRODUCTO: req.body.ID_PRODUCTO
+    };
+
+    var sql = " INSERT INTO productoxmateriaprima SET ?";
+
+    makeQuery(sql, ProdXMPData).then(() => res.json("registro insertado con exito")).catch((err) => res.status(500).json(err));
+  
+};
+
+const updateProductoxMateriaprima = (req, res = response) => {
+
+    var ProdXMPData = {
+        ID_PROD_X_MAT: req.body.ID_PROD_X_MAT,
+        ID_MATERIA_PRIMA: req.body.ID_MATERIA_PRIMA,
+        ID_PRODUCTO: req.body.ID_PRODUCTO
+    };
+
+    var sql =
+      "UPDATE productoxmateriaprima SET ID_MATERIA_PRIMA = " +
+      ProdXMPData.ID_MATERIA_PRIMA +
+      ", ID_PRODUCTO = " +
+      ProdXMPData.ID_PRODUCTO +
+      " WHERE ID_PROD_X_MAT = " +
+      ProdXMPData.ID_PROD_X_MAT +
+      ";";
+
+      makeQuery(sql).then(() => res.json("registro actualizado con exito")).catch((err) => res.status(500).json(err));
+  
+};
 //#endregion
 //#region METODO CONSULTA ID
-ProductoxMateriaprimaModelo.getProductoxMateriaprimasid = function(id, callback)
-{
-    if(connection)
-    {
-    
-        var sql = "SELECT "+
-        " F. `ID_PROD_X_MAT`,"+
-        " MP. `NOM_MATERIA_PRIMA`,   "+
-        " P. `NOM_PRODUCTO`  "+
-        " FROM `productoxmateriaprima` AS F  "+
-        " INNER JOIN `materia_prima` AS MP ON F. `ID_MATERIA_PRIMA` = MP. `ID_MATERIA_PRIMA`"+
-        " INNER JOIN `producto` AS P ON F. `ID_PRODUCTO` = P. `ID_PRODUCTO` "+
-        " WHERE F. `ID_PROD_X_MAT` = "+ connection.escape(id)+
-        " ORDER BY `ID_PROD_X_MAT`;";
+const getProductoxMateriaprimasid = (req, res = response) => {
 
-        connection.query(sql, function(error, rows){
-            if(error){
-                throw error;
-            }else{
-                callback(null, rows);
-            }
-        });
-    }
+    var id = req.params.id;
+
+    var sql =
+      "SELECT " +
+      " F. `ID_PROD_X_MAT`," +
+      " MP. `NOM_MATERIA_PRIMA`,   " +
+      " P. `NOM_PRODUCTO`  " +
+      " FROM `productoxmateriaprima` AS F  " +
+      " INNER JOIN `materia_prima` AS MP ON F. `ID_MATERIA_PRIMA` = MP. `ID_MATERIA_PRIMA`" +
+      " INNER JOIN `producto` AS P ON F. `ID_PRODUCTO` = P. `ID_PRODUCTO` " +
+      " WHERE F. `ID_PROD_X_MAT` = " +
+      id +
+      " ORDER BY `ID_PROD_X_MAT`;";
+
+      makeQuery(sql).then((result) => res.json(result)).catch((err) => res.status(500).json(err));
+  
 };
 //#endregion
+module.exports = { getProductoxMateriaprimas, insertProductoxMateriaprima, updateProductoxMateriaprima, getProductoxMateriaprimasid }
--- src/rutas/ProductoxMateriaprimaRuta.js
+++ src/rutas/ProductoxMateriaprimaRuta.js
@@ -1,74 +1,19 @@
 //#region METODO LISTAR
-const express = require('express');
-const router = express.Router();
+const {Router} = require('express');
+const router = Router();
 
-var ProductoxMateriaprimaModelo = require('../Modelos/ProductoxMateriaprimaModelo');
+const {getProductoxMateriaprimas,insertProductoxMateriaprima,updateProductoxMateriaprima,getProductoxMateriaprimasid} = require('../Modelos/ProductoxMateriaprimaModelo');
 
-module.exports = function()
-{
-    router.get("/", function(req, res)
-    {
-        ProductoxMateriaprimaModelo.getProductoxMateriaprimas(function(error,data)
-        {
-            res.status(200).json(data);
-        });
-    });
-    return router;
-}
-//#endregion
-//#region METODO INSERTAR
-router.post("/", function (req, res) {
-    var ProdXMPData = {
-        ID_PROD_X_MAT: null,
-        ID_MATERIA_PRIMA: req.body.ID_MATERIA_PRIMA,
-        ID_PRODUCTO: req.body.ID_PRODUCTO
-    };
+//Consulta produccion por materia prima en general
+router.get("/",getProductoxMateriaprimas)
 
- ProductoxMateriaprimaModelo.insertProductoxMateriaprima(ProdXMPData, function (error, data) {
-    console.log(" 44 productoxmateriaprima "+ProdXMPData.ID_MATERIA_PRIMA+" ini "
-    +ProdXMPData.ID_PRODUCTO)
-    ;
-    
-    if (data) {
-        res.status(200).json(data);
-    } else {
-        res.status(500).send({ error: "boo:(" })
-    }
-});
-});
-//#endregion
-//#region METODO MODIFICAR
-router.put("/",function(req,res){
-    var ProdXMPData = {
-        ID_PROD_X_MAT: req.body.ID_PROD_X_MAT,
-        ID_MATERIA_PRIMA: req.body.ID_MATERIA_PRIMA,
-        ID_PRODUCTO: req.body.ID_PRODUCTO
-    };
+//Insertar un nuevo registro
+router.post("/",insertProductoxMateriaprima)
 
-    //Uso de la funcion actualizar
-    ProductoxMateriaprimaModelo.updateProductoxMateriaprima(ProdXMPData, function (error, data){
-        if(data && data){
-            res.status(200).json(data);
-        }else{
-            res.status(500).send({error: "boo("});
-        }
-    });
-});
-//#endregion
-//#region METODO CONSULTA ID
-router.get("/:id", function(req, res){
-    var id = req.params.id;
+//Actualizar registros en la tabla
+router.put("/", updateProductoxMateriaprima);
 
-    if(!isNaN(id)){
-       ProductoxMateriaprimaModelo.getProductoxMateriaprimasid(id, function(error,data){
-            if(typeof data !== "undefined" && data.length > 0){
-                res.status(200).json(data);
-            }else{
-                res.json(404,{msg: "Registro no existe"});
-            }
-        });
-    }else{
-        res.status(500).json({msg: "Error"});
-    }
-});
-//#endregion
+//Consultar produccion por materia prima  por ID
+router.get("/:id", getProductoxMateriaprimasid);
+
+module.exports=router;
